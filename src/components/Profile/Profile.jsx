e70c04f0a176740790a7a5875719c6a9862e5206
Nadiia <emmideagorn@gmail.com>
2024-02-16 00:54:06 +0200

    Profile

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,31 @@
+import s from "./Profile.module.css"
+
+const Profile = ({ username, tag, location, avatar, stats }) => {
+    console.log(stats)
+    return (
+        <>
+        <div className={s.profile}>
+        <div className={s.description}>
+            <img
+                        src={avatar}
+                        alt="User avatar"
+                        className={s.avatar}
+            />
+                    <p className={s.name}>{username}</p>
+                    <p className={s.tag}>@{tag}</p>
+                    <p className={s.location}>{location}</p>
+        </div>
+
+                <ul className={s.stats}>
+                {Object.entries(stats).map(([key, value]) => {
+                    return ( <li className={s.list}>
+                        <span className={s.label}>{key}</span>
+                        <span className={s.quantity}>{value}</span>
+            </li>)
+                })}
+           
+        </ul>
+    </div>
+</>)}
+
+export default Profile;
